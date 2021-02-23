fb256a80bf3b090d4aaffbbcdfed017183baa50c
spandreas <spandreas@it.auth.gr>
2021-02-23 13:09:56 +0200

    react navbar and plot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {TopNav} from "./TopNav/TopNav";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Plot from 'react-plotly.js';
 
 function App() {
-  return (
-    <div className="App">
-      <p>hi</p>
-    </div>
-  );
+    var trace1 = {
+        type: 'bar',
+        x: [1, 2, 3, 4],
+        y: [5, 10, 2, 8],
+        marker: {
+            color: '#C8A2C8',
+            line: {
+                width: 2.5
+            }
+        }
+    };
+
+    var data = [ trace1 ];
+
+    var layout = {
+        title: 'Responsive to window\'s size!',
+        font: {size: 18}
+    };
+
+    var config = {responsive: true}
+    return (
+        <div className="App">
+            <Router>
+                <TopNav />
+                <Switch>
+                    <Route path="/home">
+                        <Plot data={data} layout={layout} config={config} />
+                    </Route>
+                    <Route path="/link">
+                        <p>link</p>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
